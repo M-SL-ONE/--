6dda737a10b71f169d045248957fc0b2f4346bf6
leah <sljinyao@qq.com>
2025-07-04 09:20:08 +0800

    测

--- 1_fs模块/001-写入文件.js
+++ 1_fs模块/001-写入文件.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // 异步写入文件
 fs.writeFile('./座右铭.txt', '三人行，必有我师焉', err => {
     if (err) {
-        console.log("写入失败")
+        console.log("写入失败！！！")
         return
     }
     console.log("写入成功")
