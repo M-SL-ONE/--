731a93bf90eb66b8af4fef3cf534986e9d78668b
leah <sljinyao@qq.com>
2025-07-04 10:37:16 +0800

    push到远程库

--- 1_fs模块/004-文件读取.js
+++ 1_fs模块/004-文件读取.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // 异步读取
 fs.readFile('./座右铭.txt',(err,data)=>{
     if(err){
-        console.log("写入失败")
+        console.log("push写入失败")
 return
     }
     console.log(data.toString())
