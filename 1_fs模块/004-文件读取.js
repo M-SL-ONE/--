52d3194908b14101d6837d736c192177a6f44cb7
leah <sljinyao@qq.com>
2025-07-04 10:39:33 +0800

    push2

--- 1_fs模块/004-文件读取.js
+++ 1_fs模块/004-文件读取.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 // 异步读取
 fs.readFile('./座右铭.txt',(err,data)=>{
     if(err){
-        console.log("push写入失败")
+        console.log("push写入失败2")
 return
     }
     console.log(data.toString())
