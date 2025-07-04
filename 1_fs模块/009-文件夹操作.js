5fedc4c4bcc4f729df50a56a5ec3ff9898929001
leah <sljinyao@qq.com>
2025-07-04 09:10:13 +0800

    文件夹操作修改

--- 1_fs模块/009-文件夹操作.js
+++ 1_fs模块/009-文件夹操作.js
@@ -19,13 +19,13 @@ const fs = require('fs');
 // })
 
 // 读取文件夹  
-// fs.readdir('../nodejs',(err,data) => {
-//     if (err) {
-//         console.log("读取失败")
-//         return
-//     }
-//     console.log(data)
-// })
+fs.readdir('../nodejs',(err,data) => {
+    if (err) {
+        console.log("读取失败")
+        return
+    }
+    console.log(data)
+})
 
 // 删除文件夹
 // fs.rmdir('./html',(err) => {
@@ -46,10 +46,10 @@ const fs = require('fs');
 // })
 
 // 删除
-fs.rm('./a',{recursive:true},(err) => {
-    if (err) {
-        console.log("删除失败")
-        return
-    }
-    console.log("删除成功")
-})
+// fs.rm('./a',{recursive:true},(err) => {
+//     if (err) {
+//         console.log("删除失败")
+//         return
+//     }
+//     console.log("删除成功")
+// })
